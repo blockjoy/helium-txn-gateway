f8519e7d9e952a4ee5d11c29135655f545ec5b16
Chris Bruce <chris@chrisbruce.com>
2022-04-24 23:02:12 +0200

    adds graceful shutdown

--- src/server.rs
+++ src/server.rs
@@ -4,8 +4,8 @@ use axum::{
     routing::{get, post},
     Router,
 };
-
 use std::sync::Arc;
+use tokio::signal::unix::{signal, SignalKind};
 use tower::ServiceBuilder;
 use tower_http::trace::TraceLayer;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
@@ -31,11 +31,50 @@ pub async fn start(config: Config) {
                 .into_inner(),
         );
 
-    tracing::debug!("listening on {}", config.server_addr);
+    let (shutdown_tx, shutdown_rx) = tokio::sync::oneshot::channel::<()>();
+
+    let signal_handler = tokio::spawn(async {
+        tokio::pin! {
+          let interrupt = signal(SignalKind::interrupt()).expect("could not open SIGINT channel");
+          let quit = signal(SignalKind::quit()).expect("could not open SIGQUIT channel");
+          let term = signal(SignalKind::terminate()).expect("could not open SIGTERM channel");
+        };
+
+        loop {
+            tokio::select! {
+              _ = (&mut interrupt).recv() => {
+                  tracing::info!("SIGINT received");
+                  break;
+              }
+              _ = (&mut quit).recv() => {
+                tracing::info!("SIGQUIT received");
+                  break;
+              }
+              _ = (&mut term).recv() => {
+                tracing::info!("SIGTERM received");
+                  break;
+              }
+            }
+        }
+
+        shutdown_tx
+            .send(())
+            .expect("could not send shutdown signal");
+    });
+    tracing::info!("Waiting for SIGTERM/SIGQUIT for graceful shutdown");
+
+    tracing::info!("listening on {}", config.server_addr);
     axum::Server::bind(&config.server_addr)
         .serve(app.into_make_service())
+        .with_graceful_shutdown(async {
+            shutdown_rx.await.ok();
+        })
         .await
-        .unwrap();
+        .expect("could not start HTTP Server.");
+
+    signal_handler
+        .await
+        .expect("error with shutdown handler task");
 }
 
 type NodePool = Arc<blockchain::NodePool>;
